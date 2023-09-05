e284a9e311e3e4748d9a1b585cb116b05f1660cb
Martin Revaj <mrevaj@frinx.io>
2023-09-05 15:20:04 +0200

    commented missing references

--- uniconfig/python/generate_rest_api.py
+++ uniconfig/python/generate_rest_api.py
@@ -11,10 +11,13 @@ from typing import Iterable
 from typing import Any
 
 DEFAULT_SOURCE_FILE = 'frinx_api/uniconfig/__init__.py'
+SOURCE_FILE_PY_PATH = '.'
 DEFAULT_OUTPUT_FILE = 'frinx_api/uniconfig/rest_api.py'
+
+COMMENT_TAG = '# '
 ENTER = '\n'
 SPACE = ' '
-TAB = '\t'
+TAB = '    '
 NAN = ''
 
 
@@ -44,58 +47,87 @@ class _cls(UniconfigRest):
     response = _response
 
 
+def _upformat(s: str, delimiters: list[str]) -> str:
+    for x in delimiters:
+        s = s.replace(x, SPACE)
+    return NAN.join([x.capitalize() for x in s.split(SPACE)])
+
+
+def _get_cls(endpoint: str, method: str, *, request: bool=False, response: bool=False) -> str:
+    base = _upformat(endpoint, ['/', '-']).split('=')[0] + method.capitalize()
+    return base + {request: 'Request', response: 'Response'}.get(True)
+
+
+def _get_service_name(endpoint: str) -> str:
+    service = endpoint.split('/')[-1]
+    match = re.search(r'\{(.*?)\}', service)
+    if match:
+        param = match.groups()[0].capitalize()
+        service = service.split('=')[0]
+        return _upformat(service, ['-']) + _upformat(param, ['-'])
+    return _upformat(service, ['-'])
+
+
+def _commented(code: str, *, lines: list[int] = []) -> str:
+    code_lines = code.split(ENTER)[:-1]
+    if code_lines:
+        if lines:
+            for line in lines:
+                if TAB in code_lines[line]:
+                    code_lines[line] = TAB + COMMENT_TAG + code_lines[line].replace(TAB, NAN)
+                else:
+                    code_lines[line] = COMMENT_TAG + code_lines[line]
+            return ENTER.join(code_lines) + ENTER
+        else:
+            return COMMENT_TAG + COMMENT_TAG.join(map(lambda s: s + ENTER, code_lines)) + ENTER
+    else:
+        return COMMENT_TAG + code
+
+
 def parse_swagger_scheme(scheme: dict[str, Any], req_res_refs: Iterable[str]
 ) -> tuple[list[str], list[str]]:
     
     uniconfig_rest = CustomTemplate(inspect.getsource(_cls))
     imports, definitions = [], []
     
-    def _upformat(s: str, delimiters: list[str]) -> str:
-        for x in delimiters:
-            s = s.replace(x, SPACE)
-        return NAN.join([x.capitalize() for x in s.split(SPACE)])
-    
-    def _get_cls(endpoint: str, method: str, *, request: bool=False, response: bool=False) -> str:
-        base = _upformat(endpoint, ['/', '-']).split('=')[0] + method.capitalize()
-        return base + {request: 'Request', response: 'Response'}.get(True)
-
-    def _get_service_name(endpoint: str) -> str:
-        service = endpoint.split('/')[-1]
-        match = re.search(r'\{(.*?)\}', service)
-        if match:
-            param = match.groups()[0].capitalize()
-            service = service.split('=')[0]
-            return _upformat(service, ['-']) + _upformat(param, ['-'])
-        return _upformat(service, ['-'])
-
-    def get_inputs(endpoint: str, spec: dict[str, Any]) -> list[dict[str, Any]]:
-        i, inputs = {}, []
-        methods = list(spec.keys())
-        for method in methods:
-            if len(methods) > 1:
-                i['cls']= _get_service_name(endpoint) + method.capitalize()
-            else:
-                i['cls']= _get_service_name(endpoint)
-            i['uri'] = endpoint
-            i['method'] = method.upper()
-
-            # TODO: comment import and class if not in source-file
-            i['request'] = _get_cls(endpoint, method, request=True)
-            i['response'] = _get_cls(endpoint, method, response=True)
-            imports.append(f'from . import {i["request"]}')
-            imports.append(f'from . import {i["response"]}')
-
-            inputs.append(i)
-        return inputs
-    
     for endpoint, spec in scheme['paths'].items():
         try:
-            [
-                definitions.append(uniconfig_rest.substitute(**i))
-                for i in get_inputs(endpoint, spec)
-            ]
+            methods, i = list(spec.keys()), {}
+            for method in methods:
+                if len(methods) > 1:
+                    i['cls']= _get_service_name(endpoint) + method.capitalize()
+                else:
+                    i['cls']= _get_service_name(endpoint)
+                i['uri'] = endpoint
+                i['method'] = method.upper()
+                i['request'] = _get_cls(endpoint, method, request=True)
+                i['response'] = _get_cls(endpoint, method, response=True)
+                
+                req_import = f'from {SOURCE_FILE_PY_PATH} import {i["request"]}'
+                res_import = f'from {SOURCE_FILE_PY_PATH} import {i["response"]}'
+
+                lines_to_comment = []
+
+                if i['request'] not in req_res_refs:
+                    imports.append(_commented(req_import))
+                    lines_to_comment.append(3)
+                else:
+                    imports.append(req_import)
+                if i['response'] not in req_res_refs:
+                    imports.append(_commented(res_import))
+                    lines_to_comment.append(4)
+                else:
+                    imports.append(res_import)
+                if lines_to_comment:
+                    definitions.append(
+                        _commented(uniconfig_rest.substitute(**i), lines=lines_to_comment)
+                    )
+                else:
+                    definitions.append(uniconfig_rest.substitute(**i))
+
         except Exception as e:
-            ... # TODO: error handling
+            # TODO: error handling
+            ... 
     return list(set(imports)), definitions
 
 
