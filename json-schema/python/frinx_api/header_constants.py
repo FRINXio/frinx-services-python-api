8f9bf3ca71f3a6757f6a17caef8aabc299ff012c
Jakub Boguščiak <jakub.bogusciak@elisapolystar.com>
2024-03-12 10:27:49 +0100

    [json-schema] Re-generate json-schemas
    Signed-off-by: Jakub Boguščiak <jakub.bogusciak@elisapolystar.com>

--- json-schema/python/frinx_api/header_constants.py
+++ json-schema/python/frinx_api/header_constants.py
@@ -1,5 +1,5 @@
 # Constants used in header types to differentiate among models
 
-device_registration = "device_registration"
-device_update = "device_update"
-device_removal = "device_removal"
+device_registration = 'device_registration'
+device_update = 'device_update'
+device_removal = 'device_removal'
