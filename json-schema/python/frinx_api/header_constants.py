59b6b4df6e8deb00b9d92a4d9feaf28ba88d86a4
Jaroslav Tóth <jaroslav.toth@elisapolystar.com>
2024-04-22 05:59:55 +0200

    FR-167 Specify Kafka header 'type' constant

--- json-schema/python/frinx_api/header_constants.py
+++ json-schema/python/frinx_api/header_constants.py
@@ -1,5 +1,6 @@
 # Constants used in header types to differentiate among models
 
+type_header = 'type'
 device_registration = 'device_registration'
 device_update = 'device_update'
 device_removal = 'device_removal'
