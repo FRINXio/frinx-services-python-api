60a731adca4073be8ff7a656a45f890c01c226e5
Marek Pastorek <marek.pastorek@elisapolystar.com>
2024-05-07 10:29:41 +0200

    [json-schema] Added schema for performance_message notifications
    - added schema for unified performance notification.
    - performance-monitor receives various performance metrics, which it then processes. After processing, it sends these metrics in a unified form to a specific topic in the Kafka broker.
    
    Signed-off-by: Marek Pastorek <marek.pastorek@elisapolystar.com>

--- json-schema/python/frinx_api/header_constants.py
+++ json-schema/python/frinx_api/header_constants.py
@@ -4,3 +4,7 @@ type_header = 'type'
 device_registration = 'device_registration'
 device_update = 'device_update'
 device_removal = 'device_removal'
+
+# Constants for recognizing the type of performance_message
+cpu_usage = 'cpu_usage'
+memory_usage = 'memory_usage'
